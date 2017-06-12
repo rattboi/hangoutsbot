f088871d4fd2a355f0eff405bd43d0612657e1d2
Bradon Kanyid <bradon@kanyid.org>
2017-06-11 23:19:05 -0700

    Added new playlist commands
    '!playlist recent' shows 5 most recent playlist
    '!playlist search' finds playlists that contain searchterm in them
    '!playlist convert' takes the place of '!convert', to convert a spotify
         playlist to a gmusic playlist, hosted by the bot.
    
    TODO: should limit results from search. Should also fail if searching
    for ''

---
+++ commands/playlist.py
@@ -0,0 +1,61 @@
+from models.command import BaseCommand
+from utils.parser import parser
+
+import logging
+import asyncio
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class Playlist(BaseCommand):
+
+    def __init__(self, name, parser, admin_required):
+        super(Playlist, self).__init__(name, parser, admin_required)
+        self.commands = {'convert': self.convert,
+                         'search': self.search,
+                         'recent': self.recent}
+
+    def convert(self, bot, args):
+        url = " ".join(args)
+        full_url = bot.gmusic.convert_spotify_embed_to_gmusic(url)
+        return "Converted: {}".format(bot.shorturl.get_short_url(full_url))
+
+    def format_playlists(self, bot, plists):
+        links = ["{}\n {}".format(p['name'],
+                                  bot.shorturl.get_short_url(p['share']))
+                 for p in plists]
+        if len(links) > 0:
+            message = "\n".join(links)
+        else:
+            message = "-- no results --"
+        return message
+
+    def search(self, bot, args):
+        """ Find playlists that match 'searchterm' """
+        term = " ".join(args)
+        plists = bot.gmusic.find_playlists(term)
+        return self.format_playlists(bot, plists)
+
+    def recent(self, bot, args):
+        """ Gets all bot playlists, and lists them with their shortlinks """
+        plists = bot.gmusic.get_newest_playlists()
+        return self.format_playlists(bot, plists)
+
+    def is_ok_command(self, command):
+        l_command = command.lower().strip()
+        return (l_command in self.commands)
+
+    @asyncio.coroutine
+    def run(self, bot, conversation, user, args):
+        parsed = self.parser.parse_known_args(args)
+        message = "** unknown command **"
+        if len(parsed[1]) > 0:
+            command = parsed[1][0]
+            if self.is_ok_command(command):
+                cmd_func = self.commands[command]
+                message = cmd_func(bot, parsed[1][1:])
+        yield from bot.send_message(conversation, message)
+
+
+command = Playlist("playlist", parser, False)
